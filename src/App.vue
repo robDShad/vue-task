f0e9f9aad299a522afc3d9254c5b439eb0e4d2d4
robDShad <robertshaidullin643@gmail.com>
2025-07-14 10:31:14 +0300

    sdf

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,43 @@
 import { ref, computed } from 'vue';
 
 const songs = ref(['First song', 'Second song', 'Third song', 'Fourth song', 'Fifth song']);
-const isMixingEnabled = ref(false)
+const isShuffling = ref(false)
 const inputVal = ref(null);
 const timer = ref(null)
 
 const shuffleList = () => {
-    const listCopy = songs.value;
-    for (let i = listCopy.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [listCopy[i], listCopy[j]] = [listCopy[j], listCopy[i]];
-    }
-    songs.value = listCopy;
+  return songs.value.sort(() => Math.random() - 0.5);
 }
 
-const turnOnMixing = computed(() => {
+const mixingInterval = computed(() => inputVal.value * 1000);
+
+const turnOnMixing = () => {
     clearInterval(timer.value)
-    if (isMixingEnabled.value) {
+    if (isShuffling.value) {
     timer.value = setInterval(() => {
         shuffleList();
-    }, inputVal.value * 1000)}
-})
+    }, mixingInterval.value)}
+};
 
-const enableDisableMixing = () => {
-    isMixingEnabled.value = !isMixingEnabled.value;
-    turnOnMixing.value;
+const handleMixing = () => {
+    if (inputVal.value <= 0) {
+        alert('Incorrect interval');
+        return
+    }
+    isShuffling.value = !isShuffling.value;
+    turnOnMixing();
 }
 
 </script>
 
 <template>
-    <div class="songs-container flex flex-col gap-2 p-5 border-2 border-black">
-        <div class="song-name " v-for="(song, index) in songs">
-        {{index + 1 + '. ' + song }}
-        </div>
-    </div>
-        <input v-model="inputVal" type="number" placeholder="Enter interval of mixes..." class="interval-input p-3 border-1 border-black hover:bg-gray-300 mr-5" @change="turnOnMixing">
-        <button class="mix-switch p-3 border-1 border-black hover:bg-gray-300" :class="inputVal < 1 ? 'pointer-events-none' : ''" @click="enableDisableMixing">Enable/disable mixing</button>
-        <p class="info mt-5">
-            {{ isMixingEnabled ? 'Mixing enabled...' : 'Mixing disabled...' }}
-        </p>
+    <ol class="songs-container flex flex-col gap-2 p-5 border-2 border-black">
+        <li class="song-name " v-for="song in songs">
+        {{ song }}
+        </li>
+    </ol>
+        <input v-model="inputVal" :disabled="isShuffling" type="number" placeholder="Enter interval of mixes..." class="interval-input p-3 border-1 border-black hover:bg-gray-300 mr-5 disabled:hover:cursor-not-allowed disabled:bg-gray-300">
+        <button class="mix-switch p-3 border-1 border-black hover:bg-gray-300" @click="handleMixing">
+            {{ isShuffling ? 'Stop mixing' : 'Start mixing' }}
+            </button> 
 </template>
