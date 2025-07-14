f741ed805fb61d4f6a7986991e5d1e7fa45776c3
robDShad <robertshaidullin643@gmail.com>
2025-07-14 11:36:35 +0300

    ad

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended', // для Vue 3 (для Vue 2 используйте 'plugin:vue/recommended')
+    '@vue/prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/attribute-hyphenation': 'off',
+    'vue/html-self-closing': ['error', {
+      html: {
+        void: 'always',
+        normal: 'always',
+        component: 'always'
+      }
+    }]
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,50 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed } from 'vue'
 
-const songs = ref(['First song', 'Second song', 'Third song', 'Fourth song', 'Fifth song']);
+const songs = ref(['First song', 'Second song', 'Third song', 'Fourth song', 'Fifth song'])
 const isShuffling = ref(false)
-const inputVal = ref(null);
+const inputVal = ref(null)
 const timer = ref(null)
 
 const shuffleList = () => {
-  return songs.value.sort(() => Math.random() - 0.5);
+  return songs.value.sort(() => Math.random() - 0.5)
 }
 
-const mixingInterval = computed(() => inputVal.value * 1000);
+const mixingInterval = computed(() => inputVal.value * 1000)
 
 const turnOnMixing = () => {
-    clearInterval(timer.value)
-    if (isShuffling.value) {
+  clearInterval(timer.value)
+  if (isShuffling.value) {
     timer.value = setInterval(() => {
-        shuffleList();
-    }, mixingInterval.value)}
-};
-
-const handleMixing = () => {
-    if (inputVal.value <= 0) {
-        alert('Incorrect interval');
-        return
-    }
-    isShuffling.value = !isShuffling.value;
-    turnOnMixing();
+      shuffleList()
+    }, mixingInterval.value)
+  }
 }
 
+const handleMixing = () => {
+  if (inputVal.value <= 0) {
+    alert('Incorrect interval')
+    return
+  }
+  isShuffling.value = !isShuffling.value
+  turnOnMixing()
+}
 </script>
 
 <template>
-    <ol class="songs-container flex flex-col gap-2 p-5 border-2 border-black">
-        <li class="song-name " v-for="song in songs">
-        {{ song }}
-        </li>
-    </ol>
-        <input v-model="inputVal" :disabled="isShuffling" type="number" placeholder="Enter interval of mixes..." class="interval-input p-3 border-1 border-black hover:bg-gray-300 mr-5 disabled:hover:cursor-not-allowed disabled:bg-gray-300">
-        <button class="mix-switch p-3 border-1 border-black hover:bg-gray-300" @click="handleMixing">
-            {{ isShuffling ? 'Stop mixing' : 'Start mixing' }}
-            </button> 
+  <ol class="songs-container flex flex-col gap-2 p-5 border-2 border-black">
+    <li class="song-name" v-for="song in songs">
+      {{ song }}
+    </li>
+  </ol>
+  <input
+    v-model="inputVal"
+    :disabled="isShuffling"
+    type="number"
+    placeholder="Enter interval of mixes..."
+    class="interval-input p-3 border-1 border-black hover:bg-gray-300 mr-5 disabled:hover:cursor-not-allowed disabled:bg-gray-300"
+  />
+  <button class="mix-switch p-3 border-1 border-black hover:bg-gray-300" @click="handleMixing">
+    {{ isShuffling ? 'Stop mixing' : 'Start mixing' }}
+  </button>
 </template>
